6f073c950541f219b5a57b4b3fd8bc5caec7f558
Val <valou7471@gmail.com>
2020-04-07 15:55:07 +0200

    p 8080

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import os
 
-cmd="/app/.heroku/python/bin/ladon-3.8-ctl testserve SOAPServer.py -v -p 80"
+cmd="/app/.heroku/python/bin/ladon-3.8-ctl testserve SOAPServer.py -v -p 8080"
 os.system(cmd)
