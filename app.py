695ff7a41e8b32cc3b7ed5e69b254406fec4ec22
Val <valou7471@gmail.com>
2020-04-07 13:41:45 +0200

    okay

--- app.py
+++ app.py
@@ -1,29 +1,4 @@
-# -*- coding: utf-8 -*-
+import os
 
-from flask import Flask, request, render_template
-from flask_restful import Resource, Api, reqparse
-
-    
-app = Flask(__name__)
-api = Api(app)
-parser = reqparse.RequestParser()
-parser.add_argument('distance')
-parser.add_argument('devise')
-
-@app.route('/')
-def index():
-    return render_template("Accueil.html")
-
-
-class CalculPrix(Resource):
-    def get(self):
-        parser.add_argument('task')
-        args = parser.parse_args()
-        
-        prix = float(args['distance']) * 0.25
-        
-        return {'prix' : prix}
-
-api.add_resource(CalculPrix, '/CalculPrix')
-    
-#app.run(debug='true')
+cmd="/home/$USER/.local/bin/ladon-3.7-ctl testserve SOAPServer.py -p 80"
+os.system(cmd)
