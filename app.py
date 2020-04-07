7a3e7406673056538a461d40ebfe1f4a0407cfb2
Val <valou7471@gmail.com>
2020-04-07 12:30:56 +0200

    c est la merde

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-import os
-from zeep import Client
+# -*- coding: utf-8 -*-
+
 from flask import Flask, request, render_template
+from flask_restful import Resource, Api, reqparse
 
-#import keyring
-import requests
-from datetime import datetime, date, time
-
+    
 app = Flask(__name__)
-
+api = Api(app)
+parser = reqparse.RequestParser()
+parser.add_argument('distance')
+parser.add_argument('devise')
 
 @app.route('/')
 def index():
-    return "ok"
+    return render_template("Accueil.html")
 
-#cmd="/home/$USER/.local/bin/ladon-3.7-ctl testserve SOAPServer.py -p 80"
-#os.system(cmd)
+
+class CalculPrix(Resource):
+    def get(self):
+        parser.add_argument('task')
+        args = parser.parse_args()
+        
+        prix = float(args['distance']) * 0.25
+        
+        return {'prix' : prix}
+
+api.add_resource(CalculPrix, '/CalculPrix')
+    
+#app.run(debug='true')
